9e67db8a73740bc896edd50a0913ec68062d9b22
riz-mbn <rizlee@mybizniche.com>
2021-08-26 11:05:07 +0800

    initial theme files

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php echo bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <link rel="icon" type="image/png" href="<?php echo MBN_ASSETS_URI ?>/img/favicon.png"> -->
+    <!-- <title><?php bloginfo('title') ?></title> -->
+
+    <?php wp_head() ?>
+
+</head>
+<body <?php body_class() ?>>
+
+<div id="wrapper"> 
+        
+    <header id="header" data-sticky-container data-toggler=".show-menu">
+        <div class="hsnav-s7 sticky" data-sticky data-options="marginTop:0">
+            <div class="navbar">
+                <a class="navlogo" href="#">
+                    <img src="<?php echo MBN_ASSETS_URI ?>/img/logo-white-small.svg" alt="" width="196" height="53">
+                </a>
+                <span class="navicon hide-for-large" data-toggle="header">mobile menu</span>
+
+                <div class="navutil">
+                    <a href="#"><i class="icn-phone circle"></i>(000) 000-000</a>
+                </div>
+
+                <nav class="navmenu show-for-large">
+                    <?php
+                        wp_nav_menu( array( 
+                            'theme_location' => 'main-menu',
+                            'menu'         => '',
+                            'container'    => 'ul',
+                            'items_wrap' => '<ul class="menu align-center dropdown" data-dropdown-menu>%3$s</ul>' ,
+                            'menu_class'   => 'menu align-center dropdown',
+                        ));
+                    ?> 
+                </nav>
+                <nav class="mobmenu hide-for-large">
+                    <ul class="menu accordion-menu" data-multi-open="false" data-accordion-menu data-submenu-toggle="true">
+                        <li class="current-menu-item"><a href="#">Home</a></li>
+                        <li><a href="#">Services</a>
+                            <ul>
+                                <li><a href="#">Services 1</a></li>
+                                <li><a href="#">Services 2</a></li>
+                                <li><a href="#">Services 3</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">News & Events</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>            
+    </header>
+    <main id="content" class="content">
---
+++ includes/public-hooks.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Add custom body class
+**/
+function mbn_body_class($classes){
+    // code here ...
+
+    return $classes;
+}
+add_filter('body_class', 'mbn_body_class');
+
+
+
+/*
+** Submenu Classes
+*/
+function mbn_submenu_classes($classes, $args){
+    // code here ...
+    
+    return $classes;
+}
+//add_filter('wp_nav_menu_items', 'mbn_submenu_classes', 10, 2);
+
+add_action( 'wp_head', 'preload_fonts' ); 
+function preload_fonts() { 
+    $url = 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,400;1,600&display=swap'; 
+    ?> 
+<link rel="dns-prefetch" href="https://fonts.gstatic.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous"> 
+<link rel="preload" href="<?php echo esc_url( $url ); ?>" as="fetch style" crossorigin="anonymous" > 
+<script type="text/javascript"> 
+!function(e,n,t){"use strict";var o="<?php echo esc_url( $url ); ?>",r="__3perf_googleFontsStylesheet";function c(e){(n.head||n.body).appendChild(e)}function a(){var e=n.createElement("link");e.href=o,e.rel="stylesheet",c(e)}function f(e){if(!n.getElementById(r)){var t=n.createElement("style");t.id=r,c(t)}n.getElementById(r).innerHTML=e}e.FontFace&&e.FontFace.prototype.hasOwnProperty("display")?(t[r]&&f(t[r]),fetch(o).then(function(e){return e.text()}).then(function(e){return e.replace(/@font-face {/g,"@font-face{font-display:swap;")}).then(function(e){return t[r]=e}).then(f).catch(a)):a()}(window,document,localStorage); 
+</script>
+    <?php
+} 
